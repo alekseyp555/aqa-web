c1c7a57ed1acafb39f21cd89e2479cad4cf7f3d8
alekseyp555 <park.aleksey@gmail.com>
2025-10-01 14:47:11 +0300

    check appveyor

--- src/test/java/ru/netology/web/OrderCardTest.java
+++ src/test/java/ru/netology/web/OrderCardTest.java
@@ -12,7 +12,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class OrderCardTest {
     private WebDriver driver;
 
@@ -26,7 +25,7 @@ public class OrderCardTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        options.addArguments("--headless");
+//        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
@@ -39,12 +38,21 @@ public class OrderCardTest {
 
     @Test
     void shouldTestValidFirstLastNameAndPhone() {
-        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Вася Пупкин");
+        //Ввод ФИО
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Вася-Пупкин");
+        //Ввод номера телефона
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79217931826");
+        //Чекбокс соглашения
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        // Продолжить
         driver.findElement(By.cssSelector("button")).click();
+        // Ожидаемый результат
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        // Фактический результат
         String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
+//      Метод trim() удаляет начальные и конечные пробельные символы строки
+//      Это полезно, когда нужно очистить строку перед обработкой или выводом её на экран.
+        //Проверяем сообщение
         assertEquals(expected, actual.trim());
     }
 
